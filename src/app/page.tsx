5fa292001da6628cea65222b02b665a5fe097df0
stevkim <stevenn.kkim@gmail.com>
2024-04-13 15:57:15 -0700

    Playing around with image component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+import Image from 'next/image';
+
 export default function Home() {
-	return <main>Testing</main>;
+	return (
+		<main>
+			<div className="w-[350px] relative h-auto min-h-[500px]">
+				<Image
+					src="https://hips.hearstapps.com/ghk.h-cdn.co/assets/17/30/2560x3839/australian-shepherd.jpg"
+					alt="aussie"
+					fill
+					sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+					priority={true}
+					className="h-auto w-full"
+				/>
+			</div>
+		</main>
+	);
 }
