f321f2d40b94a32f6381bbb5e79287e66b7c9989
stevkim <stevenn.kkim@gmail.com>
2024-04-15 17:34:41 -0700

    Added options for the editor

--- src/app/components/editor.tsx
+++ src/app/components/editor.tsx
@@ -1,12 +1,21 @@
 'use client';
-
 import 'react-quill/dist/quill.snow.css';
 import { useState } from 'react';
 import dynamic from 'next/dynamic';
 const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
 
+const options = [
+	['bold', 'italic', 'underline', 'strike'], // toggled buttons
+	['blockquote', 'code-block'], // custom button values
+	[{ list: 'ordered' }, { list: 'bullet' }],
+	[{ indent: '-1' }, { indent: '+1' }],
+	[{ size: ['small', false, 'large', 'huge'] }],
+];
+
 export default function Editor() {
-	const [value, setValue] = useState('<p>Testing</p>');
+	const [value, setValue] = useState(
+		'<p>Testingas</p><ul><li>qdasdqwd<span class="ql-size-small">asdqwdqwdqwd</span></li></ul><p><br></p><ol><li><span class="ql-size-small">aasdqwdqwd</span></li></ol><blockquote><span class="ql-size-small">asdqwdqwd</span></blockquote>'
+	);
 
 	const onSubmit = () => {
 		console.log(value);
@@ -15,10 +24,9 @@ export default function Editor() {
 	return (
 		<div>
 			<ReactQuill
-				theme="snow"
 				value={value}
-				readOnly
-				modules={{ toolbar: false }}
+				modules={{ toolbar: options }}
+				onChange={setValue}
 			/>
 			<button onClick={onSubmit}>Submit</button>
 		</div>
